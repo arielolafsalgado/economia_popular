a619a03e7ae9fa0afc9d15d070c30a9c1e62b4e0
Fede0808 <61291327+Fede0808@users.noreply.github.com>
2023-05-26 11:36:45 -0300

    Agrega etiquetas de codigo de region. Falta ajustar el formato y revisar.

--- canastas_20162022_datosgob.R
+++ canastas_20162022_datosgob.R
@@ -24,3 +24,14 @@ canastas_20162022 <- merge(cbt_aq_20162022,cba_aq_20162022)
 
 write.csv(canastas_20162022,"canastas_20162022_datosgob.csv")
 
+canastas_formato <- canastas_20162022_datosgob %>% 
+  mutate(codigo = case_when(
+    region == 'gran_buenos_aires' ~ '1',
+    region == 'noroeste' ~ '40',
+    region ==  'noreste' ~ '41',
+    region == 'cuyo' ~ '42',
+    region == 'pampeana' ~ '43',
+    region == 'patagonia' ~ '44',
+    TRUE ~ 'N/A'
+  ))
+  
