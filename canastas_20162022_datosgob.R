5d2afccd06a91771e65f4002aff9149dcd955747
fblasco1 <francisco.o.blasco@gmail.com>
2023-05-28 00:36:23 -0300

    Intento de actualización de tablero.

--- canastas_20162022_datosgob.R
+++ canastas_20162022_datosgob.R
@@ -8,17 +8,18 @@ canasta_basica_alimentaria_regiones_del_pais <- read_csv(
 canasta_basica_total_regiones_del_pais <- read_csv(
   "C:/Users/Federico/Downloads/canasta-basica-total-regiones-del-pais.csv")
 
+# Revisar forma de guardar las bases para que estén disponibles en cualquier lugar. 
 #BASES ####
 
 cba_aq_20162022 <- canasta_basica_alimentaria_regiones_del_pais %>% 
   pivot_longer(!indice_tiempo,names_to = "region",values_to="canasta") %>% 
   group_by(year(indice_tiempo),quarter(indice_tiempo),region) %>% 
-  summarise(cba=mean(canasta))  
+  summarise(CBA=mean(canasta))  
 
 cbt_aq_20162022 <- canasta_basica_total_regiones_del_pais %>% 
   pivot_longer(!indice_tiempo,names_to = "region",values_to="canasta") %>% 
   group_by(year(indice_tiempo),quarter(indice_tiempo),region) %>% 
-  summarise(cbt=mean(canasta))  
+  summarise(CBT=mean(canasta))  
 
 canastas_20162022 <- merge(cbt_aq_20162022,cba_aq_20162022)
 
@@ -33,6 +34,15 @@ canastas_formato <- canastas_20162022_datosgob %>%
     region == 'pampeana' ~ '43',
     region == 'patagonia' ~ '44',
     TRUE ~ 'N/A'
-  ))
+  )) %>% 
+  mutate(periodo =paste(`year(indice_tiempo)`, `quarter(indice_tiempo)`, sep='.')) %>% 
+  select(-c(...1,`year(indice_tiempo)`,`quarter(indice_tiempo)`)) %>% 
+  select(c(region,periodo,cba,cbt,codigo))
+
+canastas_formato <- rename(canastas_formato,CBA = cba)
+canastas_formato <- rename(canastas_formato, CBT = cbt)
+write.csv2(canastas_formato, quote = FALSE, file = "canastas.csv",row.names = FALSE)
+  
+
   
 
